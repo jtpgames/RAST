1a271dbf10a750806e3073608da82dd2b38c72a9
Juri Tomak <juri.tomak@gselectronic.com>
2024-05-22 20:19:53 +0200

    feat: added Kieker_ETL and ML_ETL submodules

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,3 +1,5 @@
 git submodule add https://github.com/jtpgames/Locust_Scripts locust_scripts
 git submodule add https://github.com/jtpgames/TeaStore TeaStore
 git submodule add https://github.com/jtpgames/Simulators Simulators
+git submodule add https://github.com/jtpgames/Kieker_ETL Kieker_ETL
+git submodule add https://github.com/jtpgames/ML_ETL ML_ETL
