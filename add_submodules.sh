02c3f1664b8bef1608f42ed41cab1aa8924ae996
Juri Tomak <juri.tomak@gselectronic.com>
2024-10-20 18:52:20 +0200

    feat(submodules): added RAST-Common-Python as submodule

--- add_submodules.sh
+++ add_submodules.sh
@@ -6,3 +6,4 @@ git submodule add https://github.com/jtpgames/Simulators Simulators
 git submodule add https://github.com/jtpgames/Kieker_ETL Kieker_ETL
 git submodule add https://github.com/jtpgames/ML_ETL ML_ETL
 git submodule add https://github.com/jtpgames/Regression-Analysis_Workload-Characterization Regression-Analysis_Workload-Characterization
+git submodule add https://github.com/jtpgames/RAST-Common-Python.git RAST-Common-Python
